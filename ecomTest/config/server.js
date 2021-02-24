5bf43c277d7be86d0e9c664c43512e3c7ee38b6c
sa902 <samanderson902@gmail.com>
2021-02-24 20:44:45 +0000

    test

---
+++ ecomTest/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '834d5747bddd0640fec30115fd2dc82b'),
+    },
+  },
+});
---
+++ ecomTest/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e6398fd-0fcf-4152-b375-09abec628822'
+};
