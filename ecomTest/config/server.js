96905a8810b4a55330bfaf2407d2019565631c69
sa902 <samanderson902@gmail.com>
2021-04-22 08:55:51 +0200

    updated

--- ecomTest/config/server.js
+++ ecomTest/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '834d5747bddd0640fec30115fd2dc82b'),
